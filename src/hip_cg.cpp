3926c226a4acb7f23450801407a1284477d17a8d
xavier <xavierabarr@gmail.com>
2026-06-23 15:24:09 -0600

    finished implementing point to point + HIP CG, but still needs to be tested

--- src/hip_cg.cpp
+++ src/hip_cg.cpp
@@ -1,6 +1,8 @@
 #include "hip_sparse_mat.hpp"
 #include "par_binary_IO.hpp"
 #include "hip_utils.hpp"
+#include <math.h>
+#include <random>
 
 
 // NOTE: this file is a work in progress for combining hip computation and point to point communication (which exists in two difference branches as no one seperated the code into different files, just over wrote the original code)
@@ -31,14 +33,14 @@ Function call to rocsparse_spmv to perform sparse matrix-vector multiplication o
 */
 void local_spmv(
     rocsparse_handle handle, rocsparse_spmat_descr A,
-    double alpha, rocsparse_dnvec_descr x, 
+    double alpha, rocsparse_dnvec_descr x,
     double beta, rocsparse_dnvec_descr y,
     size_t tmp_buffer_size, void* tmp_buffer
 ) {
     ROCSPARSE_CHECK(
         rocsparse_spmv(
             handle, rocsparse_operation_none,
-            &alpha, A, x, &beta, y, 
+            &alpha, A, x, &beta, y,
             rocsparse_datatype_f64_r,
             rocsparse_spmv_alg_default,
             rocsparse_spmv_stage_compute,
@@ -55,17 +57,17 @@ Parallel SpMV: b = alpha*A*x + beta*b (point to point version).
 @args
 - alpha: scalar multiplier for A*x
 - A: ParMat object containing the distributed matrix
-- x_d: input vector (local portion)
+- x_d: input vector (local portion, device pointer)
 - vec_x: rocsparse dense vector descriptor for input vector
 - beta: scalar multiplier for b
-- b_d: output vector (local portion)
+- b_d: output vector (local portion, device pointer)
 - vec_b: rocsparse dense vector descriptor for output vector
-- sendbuf: buffer for sending data
-- recvbuf: buffer for receiving data
+- sendbuf: device buffer for sending halo data
+- recvbuf: device buffer for receiving halo data
 - vec_recv: rocsparse dense vector descriptor for received data
 */
 void parallel_spmv(
-    double alpha, ParMat& A, double* x_d, rocsparse_dnvec_descr vec_x, 
+    double alpha, ParMat& A, double* x_d, rocsparse_dnvec_descr vec_x,
     double beta, double* b_d, rocsparse_dnvec_descr vec_b,
     double* sendbuf, double* recvbuf, rocsparse_dnvec_descr vec_recv
 ) {
@@ -106,8 +108,6 @@ void parallel_spmv(
         proc  = A.send_comm.procs[i];
         start = A.send_comm.ptr[i];
         end   = A.send_comm.ptr[i + 1];
-        for (int j = start; j < end; j++)
-            sendbuf[j] = x[A.send_comm.idx[j]];
         MPI_Isend(
             &(sendbuf[start]),
             (int)(end - start),
@@ -119,19 +119,19 @@ void parallel_spmv(
         );
     }
 
-    // perform local computation
+    // Perform local computation while communication is in flight
     local_spmv(
         A.sparse_handle,
         A.d_on_proc.descr,
         alpha,
-        vec_x, 
+        vec_x,
         beta,
         vec_b,
         A.d_on_proc.buf_size,
         A.d_on_proc.buffer
     );
 
-    // wait for all communication to complete
+    // Wait for all communication to complete
     if (A.recv_comm.n_msgs)
     {
         MPI_Waitall(A.recv_comm.n_msgs, A.recv_comm.req.data(), MPI_STATUSES_IGNORE);
@@ -142,7 +142,7 @@ void parallel_spmv(
         MPI_Waitall(A.send_comm.n_msgs, A.send_comm.req.data(), MPI_STATUSES_IGNORE);
     }
 
-    // perform off-process computation
+    // Perform off-process computation with received halo data
     local_spmv(
         A.sparse_handle,
         A.d_off_proc.descr,
@@ -156,65 +156,116 @@ void parallel_spmv(
 
 }
 
-double inner_product(std::vector<double> a, std::vector<double> b)
-{
-    double sum, sum_local;
 
-    sum_local = 0;
-    for (int i = 0; i < a.size(); i++)
-        sum_local += a[i] * b[i];
-
-    MPI_Allreduce(&sum_local, &sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-    return sum;
-}
-
-void axpy(double alpha, std::vector<double>& x, std::vector<double>& y)
-{
-    for (int i = 0; i < x.size(); i++)
-        x[i] = x[i] + alpha*y[i];
-}
-
-void scale(double alpha, std::vector<double>& x)
-{
-    for (int i = 0; i < x.size(); i++)
-        x[i] = alpha*x[i];
-}
-
-
-/* 
+/**
+Parallel inner product of two device vectors using rocBLAS ddot + MPI_Allreduce.
+@args
+- handle: rocblas handle
+- n: local number of elements
+- a_d: device pointer to first vector
+- b_d: device pointer to second vector
 */
-void CG(
+double inner_product(rocblas_handle handle, int n, double* a_d, double* b_d)
+{
+    double local_sum, global_sum;
+    rocblas_ddot(handle, n, a_d, 1, b_d, 1, &local_sum);
+    HIP_CHECK(hipStreamSynchronize(0));
+    MPI_Allreduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    return global_sum;
+}
+
+
+/**
+AXPY on device: x = x + alpha * y using rocBLAS daxpy.
+@args
+- handle: rocblas handle
+- n: number of elements
+- alpha: scalar multiplier
+- x_d: device pointer to output vector (updated in-place)
+- y_d: device pointer to input vector
+*/
+void axpy(rocblas_handle handle, int n, double alpha, double* x_d, double* y_d)
+{
+    rocblas_daxpy(handle, n, &alpha, y_d, 1, x_d, 1);
+    HIP_CHECK(hipStreamSynchronize(0));
+}
+
+
+/**
+Scale a device vector in-place: x = alpha * x using rocBLAS dscal.
+@args
+- handle: rocblas handle
+- n: number of elements
+- alpha: scalar multiplier
+- x_d: device pointer to vector (updated in-place)
+*/
+void scale(rocblas_handle handle, int n, double alpha, double* x_d)
+{
+    rocblas_dscal(handle, n, &alpha, x_d, 1);
+    HIP_CHECK(hipStreamSynchronize(0));
+}
+
+
+/**
+CG solver. Assumes x is initialized to zero and b is already computed on device.
+Returns the number of iterations taken.
+@args
+- A: distributed ParMat (matrix on both host and device)
+- x: device pointer to solution vector (must be zeroed before call)
+- vec_x: rocsparse dense vector descriptor for x
+- b: device pointer to right-hand side vector
+- vec_b: rocsparse dense vector descriptor for b
+- sendbuf: device buffer for outgoing halo data
+- recvbuf: device buffer for incoming halo data
+- vec_recv: rocsparse dense vector descriptor for recvbuf
+- final_norm: output pointer to store the final residual 2-norm
+*/
+int CG(
     ParMat& A, double* x, rocsparse_dnvec_descr vec_x,
     double* b, rocsparse_dnvec_descr vec_b,
-    double* sendbuf, double* recvbuf, rocsparse_dnvec_descr vec_recv
+    double* sendbuf, double* recvbuf, rocsparse_dnvec_descr vec_recv,
+    double* final_norm, int max_iter
 ) {
-    std::vector<double> res;s
+    std::vector<double> res;
     int rank, num_procs;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
+    // Initialize CG vectors and descriptors
+    double *r, *p, *Ap;
+    rocsparse_dnvec_descr vec_r, vec_p, vec_Ap;
 
-    // Set b to random values, x to 0
-    srand(time(NULL) + rank);
-    std::generate(x.begin(), x.end(), 
-            [&](){ return (double)(rand()) / RAND_MAX; });
-    spmv(1.0, A, x, 0.0, b);
-    std::fill(x.begin(), x.end(), 0);
-    
+    HIP_CHECK(hipMalloc((void**)&r, A.local_rows*sizeof(double)));
+    HIP_CHECK(hipMalloc((void**)&p, A.local_rows*sizeof(double)));
+    HIP_CHECK(hipMalloc((void**)&Ap, A.local_rows*sizeof(double)));
+
+    ROCSPARSE_CHECK(
+        rocsparse_create_dnvec_descr(
+        &vec_r, A.local_rows, r, rocsparse_datatype_f64_r
+        )
+    );
+    ROCSPARSE_CHECK(
+        rocsparse_create_dnvec_descr(
+        &vec_p, A.local_rows, p, rocsparse_datatype_f64_r
+        )
+    );
+    ROCSPARSE_CHECK(
+        rocsparse_create_dnvec_descr(
+        &vec_Ap, A.local_rows, Ap, rocsparse_datatype_f64_r
+        )
+    );
+
     int iter, recompute_r;
     double alpha, beta;
     double rr_inner, next_inner, App_inner;
     double norm_r, tol = 1e-6;
-    // int max_iter = ((int)(1.3*b.size())) + 2;
-    int max_iter = 500;
 
     // r0 = b - A * x0
     HIP_CHECK(
         hipMemcpyAsync(r, b, A.local_rows*sizeof(double),
             hipMemcpyDeviceToDevice, 0));
     HIP_CHECK(hipStreamSynchronize(0));
-    ptp_spmv(-1.0, A, x, 1.0, r);
+    parallel_spmv(-1.0, A, x, vec_x, 1.0, r, vec_r, sendbuf, recvbuf, vec_recv);
 
     // p0 = r0
     HIP_CHECK(hipMemcpyAsync(p, r, A.local_rows*sizeof(double),
@@ -222,7 +273,7 @@ void CG(
     HIP_CHECK(hipStreamSynchronize(0));
 
     // Find initial (r, r) and residual
-    rr_inner = inner_product(r, r);
+    rr_inner = inner_product(A.blas_handle, A.local_rows, r, r);
     norm_r = sqrt(rr_inner);
     res.push_back(norm_r);
 
@@ -236,33 +287,37 @@ void CG(
     // Main CG Loop
     while (norm_r > tol && iter < max_iter) {
         // alpha_i = (r_i, r_i) / (A*p_i, p_i)
-        ptp_spmv(1.0, A, p, 0.0, Ap);
-        App_inner = inner_product(Ap, p);
-        if (App_inner < 0.0)
-        {
+        parallel_spmv(1.0, A, p, vec_p, 0.0, Ap, vec_Ap, sendbuf, recvbuf, vec_recv);
+        App_inner = inner_product(A.blas_handle, A.local_rows, Ap, p);
+        
+        if (App_inner < 0.0) {
             printf("Indefinite matrix detected in CG! Aborting...\n");
             MPI_Abort(MPI_COMM_WORLD, -1);
         }
+
         alpha = rr_inner / App_inner;
 
-        axpy(alpha, x, p);
-
         // x_{i+1} = x_i + alpha_i * p_i
-        if ((iter % recompute_r) && iter > 0)
-        {
-            axpy(-1.0*alpha, r, Ap);
+        axpy(A.blas_handle, A.local_rows, alpha, x, p);
+
+        if ((iter % recompute_r) && iter > 0) {
+            // r_{i+1} = r_i - alpha_i * Ap_i
+            axpy(A.blas_handle, A.local_rows, -1.0*alpha, r, Ap);
         }
-        else
-        {
-            r = b;
-            spmv(-1.0, A, x, 1.0, r);
+        else {
+            // Periodically recompute r from scratch to avoid floating point drift
+            HIP_CHECK(hipMemcpyAsync(r, b, A.local_rows*sizeof(double),
+                    hipMemcpyDeviceToDevice, 0));
+            HIP_CHECK(hipStreamSynchronize(0));
+            parallel_spmv(-1.0, A, x, vec_x, 1.0, r, vec_r, sendbuf, recvbuf, vec_recv);
         }
 
-        next_inner = inner_product(r, r);
+        next_inner = inner_product(A.blas_handle, A.local_rows, r, r);
         beta = next_inner / rr_inner;
 
-        scale(beta, p);
-        axpy(1.0, p, r);
+        // p_{i+1} = r_{i+1} + beta_i * p_i
+        scale(A.blas_handle, A.local_rows, beta, p);
+        axpy(A.blas_handle, A.local_rows, 1.0, p, r);
 
         // Update next inner product
         rr_inner = next_inner;
@@ -272,35 +327,22 @@ void CG(
 
         iter++;
     }
-}
 
+    // Cleanup CG vectors and descriptors
+    ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_r));
+    ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_p));
+    ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_Ap));
+    HIP_CHECK(hipFree(r));
+    HIP_CHECK(hipFree(p));
+    HIP_CHECK(hipFree(Ap));
 
-
-/* 
-  This function reads the matrix stored in a file in par_binary format???
-  It returns the time taken to read the matrix.
-*/
-double read_matrix(const char* filename, ParMat& A) {
-    
-    return tfinal;
-}
-
-
-/* 
-  This function calls form_comm to set up the communication patterns for the matrix.
-  It returns the time taken to set up the communication patterns.
-
-*/
-double setup_comm_patterns(ParMat& A) {
-    double t0 = MPI_Wtime();
-    double tfinal;
-    
-    return tfinal;
+    *final_norm = norm_r;
+    return iter;
 }
 
 
 /* MAIN
-    @args - filename: path to matrix file in par_binary format??? (default: "Dubcova2.pm")
+    @args - filename: path to matrix file in par_binary format (default: "Dubcova2.pm")
 */
 int main(int argc, char* argv[]) {
     // Initialize MPI
@@ -310,46 +352,46 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    /* 
-      Read in the matrix 
-      
+    /*
+      Read in the matrix
+
     */
     const char* filename = "Dubcova2.pm";
     if (argc > 1) filename = argv[1];
 
     ParMat A;
     MPI_Barrier(MPI_COMM_WORLD);
-    
+
     t0 = MPI_Wtime();
-    
+
     readParMatrix(filename, A);
-    
+
     tfinal = MPI_Wtime() - t0;
     MPI_Allreduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    
-    if (rank == 0) printf("Read matrix: %e\n", read_matrix_time);
+
+    if (rank == 0) printf("Read matrix: %e\n", tfinal);
     fflush(stdout);
 
     /*
-      Form communication patterns 
-      
+      Form communication patterns
+
     */
     MPI_Barrier(MPI_COMM_WORLD);
     t0 = MPI_Wtime();
-    
-    form_comm(A);
-    
+
+    form_comm(A); // defined in hip_sparse_mat.hpp
+
     tfinal = MPI_Wtime() - t0;
     MPI_Allreduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    
-    if (rank == 0) printf("Form comm: %e\n", setup_comm_time);
+
+    if (rank == 0) printf("Form comm: %e\n", tfinal);
     fflush(stdout);
 
     /*
-      Copy matrix to device and initialize dense vector buffers 
-      
+      Copy matrix to device and initialize dense vector buffers
+
       */
-    copy_to_device(A); // this is a function in hip_sparse_mat.hpp
+    copy_to_device(A); // defined in hip_sparse_mat.hpp
 
     std::vector<double> x(A.local_cols);
     std::vector<double> b(A.local_rows);
@@ -361,13 +403,13 @@ int main(int argc, char* argv[]) {
 
     /*
       Initialize send and receive buffers
-    
+
       */
     double* sendbuf = NULL;
     double* recvbuf = NULL;
 
     if (A.send_comm.size_msgs) {
-        HIP_CHECK(hipMalloc((void**)&sendbuf, 
+        HIP_CHECK(hipMalloc((void**)&sendbuf,
                 A.send_comm.size_msgs*sizeof(double)));
     }
     if (A.recv_comm.size_msgs) {
@@ -377,7 +419,7 @@ int main(int argc, char* argv[]) {
 
     /*
       Initialize device descriptors for dense vectors x, b, r, and recv buffer
-    
+
     */
     rocsparse_dnvec_descr vec_x, vec_b, vec_r, vec_recv;
     ROCSPARSE_CHECK(
@@ -393,7 +435,7 @@ int main(int argc, char* argv[]) {
             rocsparse_datatype_f64_r)
     );
     ROCSPARSE_CHECK(
-        rocsparse_create_dnvec_descr(&vec_recv, A.recv_comm.size_msgs, 
+        rocsparse_create_dnvec_descr(&vec_recv, A.recv_comm.size_msgs,
                 recvbuf, rocsparse_datatype_f64_r)
     );
 
@@ -414,7 +456,7 @@ int main(int argc, char* argv[]) {
     // call rocsparse_spmv to figure out the buffer size needed
     ROCSPARSE_CHECK(
         rocsparse_spmv(
-            A.sparse_handle, 
+            A.sparse_handle,
             rocsparse_operation_none,
             &one, A.d_on_proc.descr, vec_x, &zero, vec_b,
             rocsparse_datatype_f64_r,
@@ -432,7 +474,7 @@ int main(int argc, char* argv[]) {
     // repeat for off-proc
     ROCSPARSE_CHECK(
         rocsparse_spmv(
-            A.sparse_handle, 
+            A.sparse_handle,
             rocsparse_operation_none,
             &one, A.d_off_proc.descr, vec_recv, &zero, vec_b,
             rocsparse_datatype_f64_r,
@@ -440,7 +482,7 @@ int main(int argc, char* argv[]) {
             rocsparse_spmv_stage_buffer_size,
             &A.d_off_proc.buf_size, NULL
         )
-    ); 
+    );
     if (A.d_off_proc.buf_size) {
         HIP_CHECK(hipMalloc(&A.d_off_proc.buffer,
                 A.d_off_proc.buf_size));
@@ -448,10 +490,10 @@ int main(int argc, char* argv[]) {
 
     /*
       Initialize x
-      
+
       Set x to random values, b = A*x
-      Will reset x to 0 before each CG 
-      
+      Will reset x to 0 before CG
+
       */
     double alpha = 1.0;
     double beta = 0.0;
@@ -468,32 +510,29 @@ int main(int argc, char* argv[]) {
         sendbuf, recvbuf, vec_recv
     );
 
+    /*
+      Run CG
+      TODO: add code to run multiple iterations and time them
+    */
+    double norm_r;
+    int max_iter = 500;
+    MPI_Barrier(MPI_COMM_WORLD);
+    HIP_CHECK(hipMemsetAsync(x_d, 0, A.local_cols*sizeof(double), 0));
+    HIP_CHECK(hipStreamSynchronize(0));
+    
+    int conv_iters = CG(A, x_d, vec_x, b_d, vec_b, sendbuf, recvbuf, vec_recv, &norm_r, max_iter);
 
-
-    // time the CG iterations
-    int n_iters;
-    int conv_iter;
-    std::vector<double> r;
-    double sum;
-    double local_norm_b, norm_b;
-    norm_b = inner_product(
-        A.blas_handle, A.local_rows, b_d,
-        b_d, &local_norm_b, &norm_b, NULL
-    );
-    norm_b = sqrt(norm_b);
-    if (rank == 0) printf("norm b %e\n", norm_b);
-
-    if (rank == 0) 
+    if (rank == 0)
     {
-        if (iter == max_iter)
+        if (conv_iters == max_iter)
             printf("Max Iterations Reached.\n");
         else
-            printf("%d Iteration required to converge\n", iter);
+            printf("%d Iterations required to converge\n", conv_iters);
         printf("2 Norm of Residual: %lg\n\n", norm_r);
     }
 
 
-    // finalize
+    // Finalize
     ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_x));
     ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_b));
     ROCSPARSE_CHECK(rocsparse_destroy_dnvec_descr(vec_r));
