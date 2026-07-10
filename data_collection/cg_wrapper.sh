7652789537b2f6c70dd73c0585583ddbf2786c59
xavier <xavierabarr@gmail.com>
2026-07-10 10:17:42 -0700

    added tags to .cali and .json output files

--- data_collection/cg_wrapper.sh
+++ data_collection/cg_wrapper.sh
@@ -11,10 +11,15 @@ export MATRIX=$6
 
 export MATRIX_PATH=$PWD/../petsc_matrices/${MATRIX}.petsc
 
+TAG="NODES-${NNODES}_PROCS-${PROCS}_RDZV-${FI_CXI_RDZV_THRESHOLD}_MATCH-${FI_CXI_RX_MATCH_MODE}_IPC-${MPICH_GPU_IPC_ENABLED}_ASYNC-${MPICH_ASYNC_PROGRESS}_ITERATIONS-${MAX_ITERS}_MAT-${MATRIX}"
+
 # caliper configs
-export CALI_CONFIG="runtime-profile(output=cg_solver.cali,profile.mpi,mpi.message.count,mpi.message.size)"
+export CALI_CONFIG="runtime-profile(output=${TAG}.cali,profile.mpi,mpi.message.count,mpi.message.size)"
+export TOPOLOGY_FILE=${TAG}.json
+
 # export CALI_SERVICES_ENABLE=debug
 # export CALI_LOG_VERBOSITY=2
+
 echo "Running CG with the following parameters:"
 echo "FI_CXI_RDZV_THRESHOLD: $FI_CXI_RDZV_THRESHOLD"
 echo "FI_CXI_RX_MATCH_MODE: $FI_CXI_RX_MATCH_MODE"
